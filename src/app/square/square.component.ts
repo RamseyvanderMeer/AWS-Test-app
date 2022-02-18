0d5da63ada1799e5ce257e1e858fed388d07e061
Ramsey <ramvandermeer@gmail.com>
2022-02-18 13:40:42 -0600

    add styling

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -3,12 +3,18 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <div >
-      <p>{{ value }}</p>
+    <div>
+      <button nbButton hero status="info" *ngIf="!value">{{ value }}</button>
+      <button nbButton hero status="success" *ngIf="value == 'X'">
+        {{ value }}
+      </button>
+      <button nbButton hero status="danger" *ngIf="value == 'O'">
+        {{ value }}
+      </button>
     </div>
   `,
   styles: [
-    'p { text-align: center; font-size: 5em; }',
+    'button { text-align: center; font-size: 5em; width: 100%; height:100%;  }',
     'div { width: 100%; height:100%; display:flex; justify-content: center; align-items: center; }',
   ],
 })
